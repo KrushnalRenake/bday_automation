d50abdde4f31a4e28e2535485e29e6a788f1893d
Krushnal <114300922+KrushnalRenake@users.noreply.github.com>
2026-03-09 20:58:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import smtplib
 
 ##################### Extra Hard Starting Project ######################
 
-EMAIL = "Krushnalcop10@gmail.com"
-PASSWORD = "afwqofi"
+EMAIL = "krushnal.renake9722@gmail.com"
+PASSWORD = "ewhelcvtlgyxiqiw"
 
 # 1. Update the birthdays.csv
 
@@ -14,16 +14,15 @@ PASSWORD = "afwqofi"
 now = dt.datetime.now()
 print(now)
 
-data = pandas.read_csv("C:\\Takeda\\Python\\Birthday Wisher_Day 32\\birthdays.csv")
+data = pandas.read_csv("birthdays.csv")
 bdays_dict = data.to_dict(orient="records")
 
 for bday in bdays_dict:
     if f"{now.month}-{now.day}" == f"{bday["month"]}-{bday["day"]}":
         letter_num = random.randint(1,3)
-        with open(f"C:\\Takeda\\Python\\Birthday Wisher_Day 32\\letter_templates\\letter_{letter_num}.txt", "r") as file:
+        with open(f"letter_templates/letter_{letter_num}.txt", "r") as file:
             letter_content = file.read()
             new_content = letter_content.replace("[NAME]",bday["name"])
-            print(new_content)
 
         with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()
